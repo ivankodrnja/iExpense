5a9ec5f8ab07e0035f70e642b4194b6eb3cf0d88
Ivan Kodrnja <ikodrnja@gmail.com>
2021-11-12 12:23:08 +0100

    dismiss the addView, save data in userDefaults

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AddView: View {
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
     @State private var name = ""
     @State private var type = "Personal"
@@ -31,6 +32,13 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save"){
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
         }
     }
 }
